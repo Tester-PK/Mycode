e449f9e91552454c8289cb4082ad0cb7606e77f9
hp <pradeepmkashid94@gmail.com>
2022-07-25 13:38:34 +0530

    my first commit 2

---
+++ SeleniumAuto/src/Browser/Test2Foresting.java
@@ -0,0 +1,10 @@
+package Browser;
+
+public class Test2Foresting {
+
+    public static void main(String []args)
+    {
+
+        System.out.println("this is my new method for commiting validation");
+    }
+}
--- SeleniumAuto/src/POM/TestingPradee.java
+++ SeleniumAuto/src/POM/TestingPradee.java
@@ -6,8 +6,7 @@ public class TestingPradee {
         public static void main(String []args)
         {
             System.out.println("done first validation");
-
-
+            System.out.println("done first validation2");
         }
 
 
